38c1e61910fdceaafaa1f147c3d5e9fba658aa4d
ndmyz <love100crazy200@gmail.com>
2019-11-18 16:25:07 +0100

    modif led

--- local/ledArduino.py
+++ local/ledArduino.py
@@ -1,2 +1,15 @@
-#TODO
+import i2cComm
+
+lettre = "a"
+matrice = [ [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,0,0,0,1]
+
+          ]  
+for ligne in matrice:
+    print(ligne) 
+    for led in ligne:
+        i2cComm.writeNumber(led)
 
